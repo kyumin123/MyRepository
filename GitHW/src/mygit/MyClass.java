beba51feec0f82c48ca96c67a4af89beaf564fc0
FUCK KYU MIN <FUCK KYU MIN@DESKTOP-GL3JUCN>
2023-12-24 23:21:44 +0900

    Change in master branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -5,6 +5,7 @@ public class MyClass {
 	main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd version");
+		System.out.println("My cahnged in master branch");
 	
 	
 	}
--- GitHW/src/mygit/YourClass.java
+++ GitHW/src/mygit/YourClass.java
@@ -1,5 +1,9 @@
 package mygit;
 
 public class YourClass {
+	public void sayHello(String toWhom) {
+		System.out.println("Hello! " + toWhom);
+		System.out.println("Your change in master branch");
+	}
 
 }
